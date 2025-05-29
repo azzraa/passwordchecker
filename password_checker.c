5ac0825ea43090e2ce9864159990dec3da8695b6
azzraaa <azrkor26@gmail.com>
2025-05-29 15:59:53 +0200

    added comments

--- password_checker.c
+++ password_checker.c
@@ -2,10 +2,12 @@
 #include <string.h>
 #include <openssl/sha.h>
 
+// Function to generate SHA-256 hash
 void sha256(const char *str, unsigned char output[SHA256_DIGEST_LENGTH]) {
     SHA256((unsigned char *)str, strlen(str), output);
 }
 
+// Function to print the hash in hexadecimal
 void print_hash(unsigned char hash[SHA256_DIGEST_LENGTH]) {
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
         printf("%02x", hash[i]);
@@ -13,11 +15,12 @@ void print_hash(unsigned char hash[SHA256_DIGEST_LENGTH]) {
 }
 
 int main() {
-    char password[128];
-    char salted[256];
+    char password[128]; // User input password
+    char salted[256]; // Buffer to hold password + salt
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    const char *salt = "random_salt";
+    const char *salt = "random_salt"; // Fixed salt for this example
 
+    // Expected hash of: "secret" + "random_salt"
     const unsigned char stored_hash[SHA256_DIGEST_LENGTH] = {
         0xfb, 0x28, 0xe1, 0x42, 0x69, 0xa7, 0x0d, 0xae,
         0xbe, 0x10, 0x17, 0x54, 0x3c, 0xec, 0x5e, 0x22,
@@ -25,12 +28,16 @@ int main() {
         0x3f, 0x99, 0xb3, 0x4f, 0x07, 0xdc, 0xfa, 0x70
     };
 
+    // Prompt user to enter a password
     printf("Enter password: ");
     scanf("%127s", password);
 
+    // Salting: combine password + salt
     snprintf(salted, sizeof(salted), "%s%s", password, salt);
+    // Hashing: generate SHA-256 hash of salted password
     sha256(salted, hash);
 
+    // Compare generated hash with stored hash
     if (memcmp(hash, stored_hash, SHA256_DIGEST_LENGTH) == 0) {
         printf("Access Granted\n");
     } else {
